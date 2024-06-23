2a20a02f58fcc060d4996d03e28b583679af4821
Nirav Parekh <niruparekh09@gmail.com>
2024-06-23 17:06:51 +0530

    Added 03-Far-Away

--- 03-Far-Away/src/App.jsx
+++ 03-Far-Away/src/App.jsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
+import Form from "./Form";
+import Logo from "./Logo";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+/* eslint-disable no-unused-vars */
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
-    <div></div>    
-  )
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems}/>
+      <PackingList items={items} />
+      <Stats />
+    </div>
+  );
 }
-
-
-function Logo(){}
-
-function Form(){}
-
-function PackingList(){}
-
-function Stats(){}
---
+++ 03-Far-Away/src/Item.jsx
@@ -0,0 +1,11 @@
+/* eslint-disable react/prop-types */
+export default function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
