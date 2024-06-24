74e6be31b1dff93c7c403a331e809e09b3b7dc93
Nirav Parekh <niruparekh09@gmail.com>
2024-06-24 10:57:29 +0530

    edited item component

--- 03-Far-Away/src/Item.jsx
+++ 03-Far-Away/src/Item.jsx
@@ -5,7 +5,7 @@ export default function Item({ item, onDeleteItems }) {
       <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button onClick={onDeleteItems(item.id)}>❌</button>
+      <button onClick={() => onDeleteItems(item.id)}>❌</button>
     </li>
   );
 }
