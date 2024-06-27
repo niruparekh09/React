0674e07d789c7abf93a09b505e040ee31c324e5d
Nirav Parekh <niruparekh09@gmail.com>
2024-06-27 08:54:36 +0530

    Changed App.jsx and Added StarRating.jsx

--- 04-NRV-Movies/src/main.jsx
+++ 04-NRV-Movies/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import StarRating from "./StarRating.jsx";
+//import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+export default function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating}</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    {/* <App /> */}
+    <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating size={25} color="red" className="test" />
+    <Test />
+  </React.StrictMode>
+);
