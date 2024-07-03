b73dca938a9a1e61ad919db4bb73fe09174d1e54
Nirav Parekh <niruparekh09@gmail.com>
2024-07-03 10:18:53 +0530

    Added MainContent.jsx and alsoo added json-server dependency

--- 06-React-Quiz/.eslintrc.cjs
+++ 06-React-Quiz/.eslintrc.cjs
@@ -17,5 +17,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'no-unused-vars': ['warn', { varsIgnorePattern: 'React' }],
+    'react/prop-types': 'off',
   },
 }
--- 06-React-Quiz/src/App.jsx
+++ 06-React-Quiz/src/App.jsx
@@ -1,9 +1,14 @@
-import DateCounter from "./DateCounter";
+import Header from './Header';
+import MainContent from './MainContent';
 
 export default function App() {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <MainContent>
+        <p>1/15</p>
+        <p>Question 1</p>
+      </MainContent>
     </div>
   );
 }
