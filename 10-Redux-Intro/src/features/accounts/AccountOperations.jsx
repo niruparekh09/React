b7888ceea87d48bce9af9ba887effae3dcc38b8a
Nirav Parekh <niruparekh09@gmail.com>
2024-07-14 16:10:22 +0530

    Used Redux in React.

--- 10-Redux-Intro/src/features/accounts/AccountOperations.jsx
+++ 10-Redux-Intro/src/features/accounts/AccountOperations.jsx
@@ -1,19 +1,45 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deposite, payloan, requestLoan, withdraw } from './accountSlice';
 
 function AccountOperations() {
-  const [depositAmount, setDepositAmount] = useState("");
-  const [withdrawalAmount, setWithdrawalAmount] = useState("");
-  const [loanAmount, setLoanAmount] = useState("");
-  const [loanPurpose, setLoanPurpose] = useState("");
-  const [currency, setCurrency] = useState("USD");
+  const [depositAmount, setDepositAmount] = useState('');
+  const [withdrawalAmount, setWithdrawalAmount] = useState('');
+  const [loanAmount, setLoanAmount] = useState('');
+  const [loanPurpose, setLoanPurpose] = useState('');
+  const [currency, setCurrency] = useState('USD');
 
-  function handleDeposit() {}
+  const dispatch = useDispatch();
+  const account = useSelector((store) => store.account);
 
-  function handleWithdrawal() {}
+  const disableLoan = account.loan > 0 ? true : false;
+  const disablePayLoan = account.balance >= account.loan ? false : true;
 
-  function handleRequestLoan() {}
+  function handleDeposit() {
+    if (!depositAmount) return;
 
-  function handlePayLoan() {}
+    dispatch(deposite(depositAmount));
+    setDepositAmount('');
+  }
+
+  function handleWithdrawal() {
+    if (!withdrawalAmount) return;
+
+    dispatch(withdraw(withdrawalAmount));
+    setWithdrawalAmount('');
+  }
+
+  function handleRequestLoan() {
+    if (!loanAmount || !loanPurpose) return;
+
+    dispatch(requestLoan(loanAmount, loanPurpose));
+    setLoanAmount('');
+    setLoanPurpose('');
+  }
+
+  function handlePayLoan() {
+    dispatch(payloan());
+  }
 
   return (
     <div>
@@ -57,19 +83,27 @@ function AccountOperations() {
             value={loanAmount}
             onChange={(e) => setLoanAmount(+e.target.value)}
             placeholder="Loan amount"
+            disabled={disableLoan}
           />
           <input
             value={loanPurpose}
             onChange={(e) => setLoanPurpose(e.target.value)}
             placeholder="Loan purpose"
+            disabled={disableLoan}
           />
-          <button onClick={handleRequestLoan}>Request loan</button>
+          <button onClick={handleRequestLoan} disabled={disableLoan}>
+            Request loan
+          </button>
         </div>
 
-        <div>
-          <span>Pay back $X</span>
-          <button onClick={handlePayLoan}>Pay loan</button>
-        </div>
+        {account.loan > 0 && (
+          <div>
+            <span>Pay back ${account.loan} </span>
+            <button onClick={handlePayLoan} disabled={disablePayLoan}>
+              Pay loan
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
