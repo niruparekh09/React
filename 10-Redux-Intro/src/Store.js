a44486ab8a11766d726995907736ada7e3a93abe
Nirav Parekh <niruparekh09@gmail.com>
2024-07-14 13:18:21 +0530

    Restructured State Slices

--- 10-Redux-Intro/src/Store.js
+++ 10-Redux-Intro/src/Store.js
@@ -1,114 +1,12 @@
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
+import accountReducer from './features/accounts/accountSlice';
+import customerReducer from './features/customers/customerSlice';
 
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: '',
-};
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer,
+});
 
-const initialStateCustomer = {
-  fullName: '',
-  nationalID: '',
-  createdAt: '',
-};
+const store = createStore(rootReducer);
 
-function accountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case 'account/deposite':
-      return { ...state, balance: state.balance + action.payload };
-
-    case 'account/withdraw':
-      return { ...state, balance: state.balance - action.payload };
-
-    case 'account/requestLoan':
-      if (state.loan > 0) return state;
-      //! LATER
-      return {
-        ...state,
-        loan: action.payload.amount,
-        loanPurpose: action.payload.purpose,
-        balance: state.balance + action.payload.amount,
-      };
-
-    case 'account/payLoan':
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: '',
-        balance: state.balance - state.loan,
-      };
-
-    default:
-      return state;
-  }
-}
-
-function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case 'customer/createCustomer':
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalID: action.payload.nationalID,
-        createdAt: action.payload.createdAt,
-      };
-
-    case 'customer/updateName':
-      return {
-        ...state,
-        fullName: action.payload,
-      };
-
-    default:
-      return state;
-  }
-}
-
-const store = createStore(customerReducer);
-
-function deposite(amount) {
-  return { type: 'account/deposite', payload: amount };
-}
-
-function withdraw(amount) {
-  return { type: 'account/withdraw', payload: amount };
-}
-
-function requestLoan(amount, purpose) {
-  return {
-    type: 'account/requestLoan',
-    payload: { amount, purpose },
-  };
-}
-
-function payloan() {
-  return { type: 'account/payLoan' };
-}
-
-// store.dispatch(deposite(500));
-// console.log(store.getState());
-// store.dispatch(withdraw(200));
-// console.log(store.getState());
-// store.dispatch(requestLoan(1000, 'Buy A Car'));
-// console.log(store.getState());
-// store.dispatch(payloan());
-// console.log(store.getState());
-
-function createCustomer(fullName, nationalID) {
-  return {
-    type: 'customer/createCustomer',
-    payload: { fullName, nationalID, createdAt: new Date().toISOString() },
-  };
-}
-
-function updateName(fullName) {
-  return {
-    type: 'customer/updateName',
-    payload: fullName,
-  };
-}
-
-store.dispatch(createCustomer('NRV', '19'));
-console.log(store.getState());
-store.dispatch(updateName('NRV_ROGERS'));
-console.log(store.getState());
+export default store;
---
+++ 10-Redux-Intro/src/features/accounts/accountSlice.js
@@ -0,0 +1,55 @@
+const initialStateAccount = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: '',
+};
+
+export default function accountReducer(state = initialStateAccount, action) {
+  switch (action.type) {
+    case 'account/deposite':
+      return { ...state, balance: state.balance + action.payload };
+
+    case 'account/withdraw':
+      return { ...state, balance: state.balance - action.payload };
+
+    case 'account/requestLoan':
+      if (state.loan > 0) return state;
+      //! LATER
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+
+    case 'account/payLoan':
+      return {
+        ...state,
+        loan: 0,
+        loanPurpose: '',
+        balance: state.balance - state.loan,
+      };
+
+    default:
+      return state;
+  }
+}
+
+export function deposite(amount) {
+  return { type: 'account/deposite', payload: amount };
+}
+
+export function withdraw(amount) {
+  return { type: 'account/withdraw', payload: amount };
+}
+
+export function requestLoan(amount, purpose) {
+  return {
+    type: 'account/requestLoan',
+    payload: { amount, purpose },
+  };
+}
+
+export function payloan() {
+  return { type: 'account/payLoan' };
+}
