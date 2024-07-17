4d98332ce883a821ac1e6ce4d7a8cb3574a09587
Nirav Parekh <niruparekh09@gmail.com>
2024-07-17 11:21:26 +0530

    Added all the components and added base rounting (v6.4+)

--- 11-Fast-React-Pizza/src/App.jsx
+++ 11-Fast-React-Pizza/src/App.jsx
@@ -1,5 +1,40 @@
-import React from 'react';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+      },
+    ],
+  },
+]);
 
 export default function App() {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 }
---
+++ 11-Fast-React-Pizza/src/ui/Header.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Pizza Co.</Link>
+      <p>NRV</p>
+    </div>
+  );
+}
